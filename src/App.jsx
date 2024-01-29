16b8a701fa706568645b7a018eb0fad1be238a34
21MoududAhammed <21moudud@gmail.com>
2024-01-29 11:48:00 +0600

    sidebar has done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import Header from "./components/Header";
+import Sidebar from "./components/Sidebar";
 
-export default function App(){
-    return (
-      <>
-      <Header/>
-      </>
-
-    );
+export default function App() {
+  return (
+    <>
+      <Header />
+      <main>
+        <div className="container grid lg:grid-cols-[218px_1fr] gap-[3.5rem]">
+          <Sidebar />
+        </div>
+      </main>
+    </>
+  );
 }
