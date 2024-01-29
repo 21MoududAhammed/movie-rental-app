7e90f1c7bb67ca99b19b6ea4cf158f6a7c351dd9
21MoududAhammed <21moudud@gmail.com>
2024-01-29 17:10:20 +0600

    movie details modal is done

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,34 +1,39 @@
-
+import { useState } from "react";
 import { getImageUrl } from "../utils/cine-utils";
+import MovieDetailsModal from "./MovieDetailsModal";
 import Rating from "./Rating";
 
-export default function MovieCard({movie}){
-    return (
-        <figure
-      
-        className="p-4 border border-black/10 shadow-sm dark:border-white/10 rounded-xl"
-      >
-        <img
-          className="w-full object-cover"
-          src={getImageUrl(movie.cover)}
-          alt=""
-        />
-        <figcaption className="pt-4">
-          <h3 className="text-xl mb-1">{movie.title}</h3>
-          <p className="text-[#575A6E] text-sm mb-2">
-            {movie.description}
-          </p>
-          <div className="flex items-center space-x-1 mb-5">
-            <Rating value={movie.rating}/>
-          </div>
-          <a
-            className="bg-primary rounded-lg py-2 px-5 flex items-center justify-center gap-2 text-[#171923] font-semibold text-sm"
-            href="#"
-          >
-            <img src="./assets/tag.svg" alt="" />
-            <span>$ {movie.price} | Add to Cart</span>
-          </a>
-        </figcaption>
+export default function MovieCard({ movie }) {
+  const [isShow, setIsShow] = useState(false);
+  function handleCancelModal (){
+    setIsShow(false);
+  }
+  return (
+    <>
+      {isShow && <MovieDetailsModal movie={movie} onCancelModal={handleCancelModal} />}
+      <figure className="p-4 border border-black/10 shadow-sm dark:border-white/10 rounded-xl">
+        <a onClick={()=>{setIsShow(true)}} href="#">
+          <img
+            className="w-full object-cover"
+            src={getImageUrl(movie.cover)}
+            alt=""
+          />
+          <figcaption className="pt-4">
+            <h3 className="text-xl mb-1">{movie.title}</h3>
+            <p className="text-[#575A6E] text-sm mb-2">{movie.description}</p>
+            <div className="flex items-center space-x-1 mb-5">
+              <Rating value={movie.rating} />
+            </div>
+            <a
+              className="bg-primary rounded-lg py-2 px-5 flex items-center justify-center gap-2 text-[#171923] font-semibold text-sm"
+              href="#"
+            >
+              <img src="./assets/tag.svg" alt="" />
+              <span>$ {movie.price} | Add to Cart</span>
+            </a>
+          </figcaption>
+        </a>
       </figure>
-    );
+    </>
+  );
 }
